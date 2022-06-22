c12082c162496efc7ddb36f4acd23972e2bc0eb3
Christian Casey <christiancasey86@gmail.com>
2022-06-22 08:51:35 +0200

    Fixed touch events

---
+++ search.js
@@ -0,0 +1,29 @@
+//   ,ggg,        gg                                                               
+//  dP""Y8b       88                                          I8                   
+//  Yb, `88       88                                          I8                   
+//   `"  88       88   gg                                  88888888  gg            
+//       88aaaaaaa88   ""                                     I8     ""            
+//       88"""""""88   gg    ,ggg,    ,gggggg,    ,gggg,gg    I8     gg     ,gggg, 
+//       88       88   88   i8" "8i   dP""""8I   dP"  "Y8I    I8     88    dP"  "Yb
+//       88       88   88   I8, ,8I  ,8'    8I  i8'    ,8I   ,I8,    88   i8'      
+//       88       Y8,_,88,_ `YbadP' ,dP     Y8,,d8,   ,d8b, ,d88b, _,88,_,d8,_    _
+//       8,gg,    `Y88P""Y8888P"Y8888P      `Y8P"Y8888P"`Y888P""Y888P""Y8P""Y8888PP
+//       i8""8i                                                                    
+//       `8,,8'                                                                    
+//        `88'     gg                                                              
+//        dP"8,    ""                                                              
+//       dP' `8a   gg     ,gggg,gg   ,ggg,,ggg,                                    
+//      dP'   `Yb  88    dP"  "Y8I  ,8" "8P" "8,                                   
+//  _ ,dP'     I8  88   i8'    ,8I  I8   8I   8I                                   
+//  "888,,____,dP_,88,_,d8,   ,d8I ,dP   8I   Yb,                                  
+//  a8P"Y88888P" 8P""Y8P"Y8888P"8888P'   8I   `Y8                                  
+//        ,gg,                ,d8I'                                                
+//       i8""8i             ,dP'8I                          ,dPYb,                 
+//       `8,,8'            ,8"  8I                          IP'`Yb                 
+//        `88'             I8   8I                          I8  8I                 
+//        dP"8,            `8, ,8I                          I8  8'                 
+//       dP' `8a   ,ggg,    `,gggg,gg   ,gggggg,    ,gggg,  I8 dPgg,               
+//      dP'   `Yb i8" "8i   dP"  "Y8I   dP""""8I   dP"  "Yb I8dP" "8I              
+//  _ ,dP'     I8 I8, ,8I  i8'    ,8I  ,8'    8I  i8'       I8P    I8              
+//  "888,,____,dP `YbadP' ,d8,   ,d8b,,dP     Y8,,d8,_    _,d8     I8,             
+//  a8P"Y88888P" 888P"Y888P"Y8888P"`Y88P      `Y8P""Y8888PP88P     `Y8  
