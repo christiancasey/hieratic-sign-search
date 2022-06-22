af8ee2edbfda98fc0cb9a140aa957b3991ae7a61
Christian Casey <christiancasey86@gmail.com>
2022-06-22 18:28:42 +0200

    Edge finding working

--- search.js
+++ search.js
@@ -27,3 +27,100 @@
 //  _ ,dP'     I8 I8, ,8I  i8'    ,8I  ,8'    8I  i8'       I8P    I8              
 //  "888,,____,dP `YbadP' ,d8,   ,d8b,,dP     Y8,,d8,_    _,d8     I8,             
 //  a8P"Y88888P" 888P"Y888P"Y8888P"`Y88P      `Y8P""Y8888PP88P     `Y8  
+
+let canvas = document.getElementById("signPad");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FFFFFF";
+ctx.fillRect(0,0,canvas.width, canvas.height);
+ctx.strokeStyle = "#FF0000";
+ctx.lineWidth = 18;
+// ctx.strokeRect(20, 20, 150, 100);
+
+const rect = canvas.getBoundingClientRect();
+let image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+const getCursorPosition = (canvas, event) => {
+  let x, y;
+  // For touch events
+  if (event.touches) {
+    x = event.touches[0].clientX - rect.left;
+    y = event.touches[0].clientY - rect.top;
+  } else {
+    x = event.clientX - rect.left;
+    y = event.clientY - rect.top;
+  }
+  return {x: x, y: y};
+};
+
+let mouseDown = false;
+
+const down = e => {
+  mouseDown = true;
+  ctx.beginPath();
+  e.preventDefault();
+};
+
+const up = e => {
+  mouseDown = false;
+  console.log('Run search');
+  runSearch();
+};
+
+const move = e => {
+  let coords = getCursorPosition(canvas, e);
+  
+  if (mouseDown) {
+    ctx.lineTo(coords.x, coords.y);
+    ctx.stroke();
+    
+    image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  }
+  
+  e.preventDefault();
+};
+
+canvas.addEventListener('mousedown', down);
+canvas.addEventListener('touchstart', down);
+canvas.addEventListener('mousemove', move);
+canvas.addEventListener('touchmove', move);
+canvas.addEventListener('mouseup', up);
+canvas.addEventListener('touchend', up);
+
+
+const clearPad = e => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+}
+
+console.log(image);
+
+const runSearch = () => {
+  // let imageEdge = new MarvinImage(image.data);
+  let imageEdge = new MarvinImage();
+  // console.log(canvas.toDataURL());
+  imageEdge.load(canvas.toDataURL(),  () => {
+    let imageOut = new MarvinImage(imageEdge.getWidth(), imageEdge.getHeight());
+    imageOut.clear(0xFF000000);
+    
+    // Marvin.invertColors(imageEdge, imageOut);
+    // Marvin.invertColors(imageOut, imageOut);
+    // Marvin.blackAndWhite(imageOut, imageOut, 100);
+    // Marvin.prewitt(imageOut, imageOut, 0.9);
+    // Marvin.thresholding(imageOut, imageOut, 150);
+    // imageOut.draw(document.getElementById("filtered"));
+    
+    // Marvin.colorChannel(imageEdge, imageEdge, 14, 0, -8);
+    
+    Marvin.prewitt(imageEdge, imageOut);
+    Marvin.blackAndWhite(imageOut, imageOut, 100);
+    imageOut.draw(document.getElementById("filtered"));
+    
+    
+  });
+}
+
+
+
+
+
